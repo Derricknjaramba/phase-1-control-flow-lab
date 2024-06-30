2cf5b3ba06ceea5bdd518b24abf340516bf71698
Derricknjaramba <njarambaderrick@gmail.com>
2024-06-30 08:40:44 +0300

    controlflow

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 function scuberGreetingForFeet(){
-  // Write your code here!
+let scuberGreetingForFeet=199;
+if(scuberGreetingForFeet<=400){
+  console.log("This one is o me");
+}
+let scuberGreetingForFeet=1500;
+if(scuberGreetingForFeet>2000){
+  console.log("i will gladly take your thirty bucks");
+}
+let scuberGreetingForFeet=2501;
+if(scuberGreetingForFeet> 2500)
+  [
+    console.log('Ok, sounds good.')
+  ]
 }
 
 function ternaryCheckCity(){
-  // Write your code here!
+let city=NYC;
+if(city=NYC){
+  console.log("Ok ,sounds good");
+}
+let city=Pittsburgh;
+if (city!=NYC){
+  console.log("No go");
+}
 }
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+let switchOnCharmFromTip="generous";
+if(switchOnCharmFromTip=generous){
+  console.log("Thank you so much");
+}
+let switchOnCharmFromTip="not as generous";
+if(switchOnCharmFromTip=not as generous){
+  console.log("Thank you");
+}
+let switchOnCharmFromTip="thanks for everything";
+  if(switchOnCharmFromTip=thanks for everything){
+    console.log("Bye");
+  }
 }
